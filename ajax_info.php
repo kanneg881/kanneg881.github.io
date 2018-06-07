cc1a353ea51f6b136bfda9bc3ff903f253adacac
Enjoy <kanneg881@gmail.com>
2018-06-07 23:35:18 +0800

    amp 測試

--- ajax_info.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jackson
- * Date: 2018/1/27
- * Time: 上午2:38
- */
-
-echo "ajax test";
--- asset/javascript/test.js
+++
@@ -1,9 +0,0 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        document.getElementById("ajax").innerHTML =
-            this.responseText;
-    }
-};
-xhttp.open("GET", "ajax_info.php", true);
-xhttp.send();
